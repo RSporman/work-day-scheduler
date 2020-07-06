86c02b01de0c43e435ba591952a5961e0ef45318
Bobby Sporman <bobby.sporman@yahoo.com>
2020-07-06 14:32:30 -0400

    Update

--- script.js
+++ script.js
@@ -4,15 +4,39 @@ function secondUpdater() {
 }
 secondUpdater();
 
+// function getLocalStorage() {
+//     $(".button").each(function (elem) {
+//         $(this).parent().siblings().children('.col-md-9').val(localStorage.getItem($(this).attr("class")))
 
-function getLocalStorage() {
-    $(".button").each(function (elem) {
-        $(this).parent().siblings().children('.col-md-9').val(localStorage.getItem($(this).attr("class")));
-        // let value = localStorage.getItem(key);
-        // console.log(value);
-        // if (value) {
-        //     $(`#text${key}`).text(value);
-        // }
-    });
+for (var i = 7; i < 18; i++) {
+    var hour = $('.description' + i);
+    if (i < currentTime) {
+        hour.addClass("past");
+    } else if (i === currentTime) {
+        hour.addClass("present");
+    } else {
+        hour.addClass("future");
+    }
 }
-getLocalStorage()
+
+var saveBtn = $('.button');
+$(".button").on('click', function () {
+    console.log(this)
+    var eventId = $(this).attr('id');
+    var eventText = $(this).parent().siblings().children('.description').val();
+    console.log(eventId);
+    console.log(eventText);
+    localStorage.setItem(eventId, eventText);
+});
+
+
+
+//         // let value = localStorage.getItem(key);
+//         // console.log(value);
+//         // if (value) {
+//         //     $(`#text${key}`).text(value);
+//         // }
+//     });
+// }
+// getLocalStorage()
+
