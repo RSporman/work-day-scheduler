7cabdfef93f5e8827d556cb26c7d8704447a6898
Bobby Sporman <bobby.sporman@yahoo.com>
2020-07-05 20:36:21 -0400

    Update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-$("#demo").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+function secondUpdater() {
+    $(".time").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    setInterval(secondUpdater, 1000)
+}
+secondUpdater();
+
+
+function getLocalStorage() {
+    $(".button").each(function (elem) {
+        $(this).parent().siblings().children('.col-md-9').val(localStorage.getItem($(this).attr("class")));
+        // let value = localStorage.getItem(key);
+        // console.log(value);
+        // if (value) {
+        //     $(`#text${key}`).text(value);
+        // }
+    });
+}
+getLocalStorage()
