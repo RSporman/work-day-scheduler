0ff026700c58a7a1587e00bb2fc7cf63c1f4c76f
Bobby Sporman <bobby.sporman@yahoo.com>
2020-07-06 15:02:17 -0400

    UPDATE

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ secondUpdater();
 //     $(".button").each(function (elem) {
 //         $(this).parent().siblings().children('.col-md-9').val(localStorage.getItem($(this).attr("class")))
 
-for (var i = 7; i < 18; i++) {
+function toStandardTime(militaryTime) {
+    militaryTime = militaryTime.split(':');
+    if (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2) {
+      return (militaryTime[0] - 12) + ':' + militaryTime[1] + ':' + militaryTime[2] + ' P.M.';
+    } else {
+      return militaryTime.join(':') + ' A.M.';
+    }
+  }
+
+for (var i = 8; i < 18; i++) {
     var hour = $('.description' + i);
     if (i < currentTime) {
         hour.addClass("past");
@@ -20,12 +29,12 @@ for (var i = 7; i < 18; i++) {
 }
 
 var saveBtn = $('.button');
-$(".button").on('click', function () {
+saveBtn.on('click', function () {
     console.log(this)
     var eventId = $(this).attr('id');
-    var eventText = $(this).parent().siblings().children('.description').val();
-    console.log(eventId);
-    console.log(eventText);
+    var eventText = $(this).parent().siblings().children('#description').val();
+    // console.log(eventId);
+    // console.log(eventText);
     localStorage.setItem(eventId, eventText);
 });
 
